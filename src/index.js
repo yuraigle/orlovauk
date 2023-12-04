71c431df1339330064f8b72957955fdf45f3b8bd
yuraigle <yuraigle@gmail.com>
2023-12-04 20:07:23 +0800

    a href image

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './index.html';
 import './index.scss';
+import './img/pic1.webp';
+import './img/pic2.webp';
+import './img/pic3.webp';
+import './img/pic4.webp';
+import './img/pic5.webp';
--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,99 @@
 const fs = require("fs");
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const mode = process.env.NODE_ENV || 'development';
-const devmode = mode === 'development';
-const target = devmode ? 'web' : 'browserslist';
-const devtool = devmode ? 'source-map' : undefined;
+const mode = process.env.NODE_ENV || "development";
+const devmode = mode === "development";
+const target = devmode ? "web" : "browserslist";
+const devtool = devmode ? "source-map" : undefined;
 
 module.exports = {
-    mode,
-    target,
-    devtool,
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
-        assetModuleFilename: '[name]_[contenthash][ext][query]',
-        publicPath: '',
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: 'html-loader',
-                options: {
-                    preprocessor: processHtmlLoader
-                }
-            },
-            {
-                test: /\.(c|sa|sc)ss$/i,
-                use: [
-                    devmode ? "style-loader" : MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader",
-                    "postcss-loader"
-                ],
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html'),
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css',
-        }),
+  mode,
+  target,
+  devtool,
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    publicPath: "",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {
+          esModule: false,
+          preprocessor: processHtmlLoader,
+          sources: {
+            list: [
+              "...",
+              {
+                tag: "a",
+                attribute: "href",
+                type: "src",
+              },
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [
+          devmode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|webp)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "img/[contenthash][ext]",
+        },
+      },
     ],
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+  ],
+};
 
 // https://github.com/webpack-contrib/html-loader/issues/291#issuecomment-877797830
 function processNestedHtml(content, loaderContext, resourcePath = "") {
-    let fileDir = (resourcePath === "") ? path.dirname(loaderContext.resourcePath) : path.dirname(resourcePath)
-    const INCLUDE_PATTERN = /\<include src=\"(\.\/)?(.+)\"\/?\>(?:\<\/include\>)?/gi;
+  let fileDir =
+    resourcePath === ""
+      ? path.dirname(loaderContext.resourcePath)
+      : path.dirname(resourcePath);
+  const INCLUDE_PATTERN =
+    /\<include src=\"(\.\/)?(.+)\"\/?\>(?:\<\/include\>)?/gi;
 
-    function replaceHtml(match, pathRule, src) {
-        if (pathRule === "./") {
-            fileDir = loaderContext.context
-        }
-        const filePath = path.resolve(fileDir, src)
-        loaderContext.dependency(filePath)
-        const html = fs.readFileSync(filePath, 'utf8')
-        return processNestedHtml(html, loaderContext, filePath)
+  function replaceHtml(match, pathRule, src) {
+    if (pathRule === "./") {
+      fileDir = loaderContext.context;
     }
+    const filePath = path.resolve(fileDir, src);
+    loaderContext.dependency(filePath);
+    const html = fs.readFileSync(filePath, "utf8");
+    return processNestedHtml(html, loaderContext, filePath);
+  }
 
-    if (!INCLUDE_PATTERN.test(content)) {
-        return content
-    } else {
-        return content.replace(INCLUDE_PATTERN, replaceHtml);
-    }
+  if (!INCLUDE_PATTERN.test(content)) {
+    return content;
+  } else {
+    return content.replace(INCLUDE_PATTERN, replaceHtml);
+  }
 }
 
 function processHtmlLoader(content, loaderContext) {
-    let newContent = processNestedHtml(content, loaderContext)
-    return newContent
+  let newContent = processNestedHtml(content, loaderContext);
+  return newContent;
 }
