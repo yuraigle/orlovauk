dfc99fbcc8afc7ccd3ae46e5111e6ea3e8b4ce3b
yuraigle <yuraigle@gmail.com>
2023-12-09 14:24:27 +0800

    fslightbox->photoswipe

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import './index.html';
 import './index.scss';
 
-require("fslightbox");
+import PhotoSwipe from 'photoswipe';
+import PhotoSwipeLightbox from "photoswipe/lightbox";
+import PhotoSwipeDynamicCaption from "photoswipe-dynamic-caption-plugin";
+import 'photoswipe/style.css';
+import 'photoswipe-dynamic-caption-plugin/photoswipe-dynamic-caption-plugin.css';
+
+const lightbox1 = new PhotoSwipeLightbox({
+    gallery: '#gallery1',
+    children: 'a',
+    showHideAnimationType: 'zoom',
+    pswpModule: PhotoSwipe,
+});
+
+const lightbox2 = new PhotoSwipeLightbox({
+    gallery: '#gallery2',
+    children: 'a',
+    showHideAnimationType: 'none',
+    pswpModule: PhotoSwipe,
+});
+
+const captionPlugin = new PhotoSwipeDynamicCaption(lightbox1, {
+    type: 'auto',
+});
+
+lightbox1.init();
+lightbox2.init();
 
 document.getElementById('enroll_form').addEventListener('submit', function(ev) {
     ev.preventDefault();
